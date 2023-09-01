573b62a9b4a230b3354fcf17ca5c8b3727b9b41c
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2023-09-01 14:03:04 -0300

    Ajustes componente login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { PoPageLoginLiterals } from '@po-ui/ng-templates';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,23 @@ export class LoginComponent {
   minlength: number = 8;
 
   constructor(private _authService: AuthService){}
+
+ 
+get customLiterals() {
+  const customLiterals: PoPageLoginLiterals = {
+    welcome: 'Bem vindo ao CepViewer',
+    loginPlaceholder: 'Insira seu usuário de acesso',
+    loginErrorPattern:'Insira um email-válido',
+    loginHint:'',
+    passwordPlaceholder: 'Insira sua senha de acesso',
+    passwordErrorPattern: 'Mínimo de 8 caracteres',
+    
+    submitLabel: 'Acessar sistema'
+  };
+
+  return customLiterals
+}
+  
 
   loginSubmit(event: any) {
     this._authService.login(event).subscribe(res => {
